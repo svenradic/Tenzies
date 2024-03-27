7fa8b1304e8ef06792f822ba2d31661f2f35ab8d
svenradic <radic.sven@gmail.com>
2024-03-27 17:39:49 +0100

    add Die component alter App.jsx and style.css

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Die from "./Die.jsx"
+
+function App(){
+  return (
+    <main >
+      <div className="container">
+        <Die value="1"></Die>
+        <Die value="2"></Die>
+        <Die value="3"></Die>
+        <Die value="4"></Die>
+        <Die value="5"></Die>
+        <Die value="6"></Die>
+        <Die value="7"></Die>
+        <Die value="8"></Die>
+        <Die value="9"></Die>
+        <Die value="10"></Die>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Die.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Die (props){
+  return (
+    <div className="die">
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  )
+}
+
+export default Die
